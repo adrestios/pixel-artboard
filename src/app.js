a4e5a03265fc0708cbbc817f882ee03cb1a2af83
adrestios <778158202@qq.com>
2019-06-15 21:15:39 +0800

    后段用二进制数据保存图片，前端将二进制数据转为canvas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const path = require('path')
 const ws = require('ws')
 const express = require('express')
+const Jimp = require('jimp')
+
 
 const port = 9095
 
@@ -10,60 +12,73 @@ const app = express()
 const server = http.createServer(app)
 const wss = new ws.Server({server})
 
-const width = 50
-const height = 50
-let pixelData
-try {
-  pixelData = require('./pixel.json')
-} catch(e) {
-  pixelData = new Array(height).fill(0).map(it => new Array(width).fill('white'))
+const width = 256
+const height = 256
+
+main()
+
+async function main() {
+  let pixelData
+  try {
+    console.log(11)
+    img = await Jimp.read(path.join(__dirname, './pixel.png'))
+  } catch(e) {
+    console.log(e)
+    console.log(22)
+    img = new Jimp(width,height,0xffffffff)
+  }
+
+  setInterval(() => {
+    img.write(path.join(__dirname,'./pixel.png'), () => {
+      console.log('data has saved')
+    })
+  }, 3000)
+
+  wss.on('connection', (ws, req) => {
+    img.getBuffer(Jimp.MIME_PNG, (err, buf) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(buf)
+        ws.send(buf)
+      }
+    })
+
+    let lastDraw = 0
+
+    ws.on('message', msg => {
+      msg = JSON.parse(msg)
+      let now = Date.now()
+
+      if (msg.type === 'drawDot') {
+        if (now - lastDraw < 10) {
+          return
+        }
+        if (msg.x >= 0 && msg.x < width && msg.y >= 0 && msg.y < height) {
+
+          lastDraw = now
+          img.setPixelColor(Jimp.cssColorToHex(msg.color), msg.x, msg.y)
+          wss.clients.forEach(client => {
+            client.send(JSON.stringify({
+              type: 'updateDot',
+              x: msg.x,
+              y: msg.y,
+              color: msg.color,
+            }))
+          })
+        }
+      }
+    })
+  })
+
+  app.use(express.static(path.join(__dirname, './static')))
+
+  server.listen(port, () => {
+    console.log('server listen on port ', port)
+  })
 }
 
-setInterval(() => {
-  fs.writeFile(path.join(__dirname, './pixel.json'), JSON.stringify(pixelData), (err) => {
-    console.log('data has saved')
-  })
-}, 3000)
 
-wss.on('connection', (ws, req) => {
-  ws.send(JSON.stringify({
-    type: 'init',
-    pixelData: pixelData,
-  }))
-
-  let lastDraw = 0
-
-  ws.on('message', msg => {
-    msg = JSON.parse(msg)
-    let now = Date.now()
-
-    if (msg.type === 'drawDot') {
-      if (now - lastDraw < 10) {
-        return
-      }
-      if (msg.x >= 0 && msg.x < width && msg.y >= 0 && msg.y < height) {
-
-        lastDraw = now
-
-        pixelData[msg.y][msg.x] = msg.color
-        wss.clients.forEach(client => {
-          client.send(JSON.stringify({
-            type: 'updateDot',
-            x: msg.x,
-            y: msg.y,
-            color: msg.color,
-          }))
-        })
-      }
-    }
-  })
-})
-
-app.use(express.static(path.join(__dirname, './static')))
-
-server.listen(port, () => {
-  console.log('server listen on port ', port)
-})
 
 
 
