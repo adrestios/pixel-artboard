769a1164208fd458fd0fe1a59db8509eee89b3c0
adrestios <778158202@qq.com>
2019-06-15 15:37:36 +0800

    使用canvas来渲染

--- src/app.js
+++ src/app.js
@@ -10,10 +10,20 @@ const app = express()
 const server = http.createServer(app)
 const wss = new ws.Server({server})
 
-const pixelData = [
-  ['red','red','red'],
-  ['green', 'green', 'green'],
-]
+const width = 50
+const height = 50
+let pixelData
+try {
+  pixelData = require('./pixel.json')
+} catch(e) {
+  pixelData = new Array(height).fill(0).map(it => new Array(width).fill('white'))
+}
+
+setInterval(() => {
+  fs.writeFile(path.join(__dirname, './pixel.json'), JSON.stringify(pixelData), (err) => {
+    console.log('data has saved')
+  })
+}, 3000)
 
 wss.on('connection', (ws, req) => {
   ws.send(JSON.stringify({
@@ -21,19 +31,30 @@ wss.on('connection', (ws, req) => {
     pixelData: pixelData,
   }))
 
+  let lastDraw = 0
+
   ws.on('message', msg => {
     msg = JSON.parse(msg)
+    let now = Date.now()
 
     if (msg.type === 'drawDot') {
-      pixelData[msg.y][msg.x] = msg.color
-      wss.clients.forEach(client => {
-        client.send(JSON.stringify({
-          type: 'updateDot',
-          x: msg.x,
-          y: msg.y,
-          color: msg.color,
-        }))
-      })
+      if (now - lastDraw < 1000) {
+        return
+      }
+      if (msg.x >= 0 && msg.x < width && msg.y >= 0 && msg.y < height) {
+
+        lastDraw = now
+
+        pixelData[msg.y][msg.x] = msg.color
+        wss.clients.forEach(client => {
+          client.send(JSON.stringify({
+            type: 'updateDot',
+            x: msg.x,
+            y: msg.y,
+            color: msg.color,
+          }))
+        })
+      }
     }
   })
 })
